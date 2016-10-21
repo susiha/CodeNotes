286a8f68f0df514a861ce37a68b2da6e034ebc94
sunshihai <shihaisun@creditease.cn>
2016-10-21 13:48:46 +0800

    add recycleView

---
+++ app/src/main/java/com/susiha/codenotes/adapter/MainRecycleAdapter.java
@@ -0,0 +1,28 @@
+package com.susiha.codenotes.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * Created by 苏斯哈 on 2016/10/21.
+ */
+
+public class MainRecycleAdapter extends RecyclerView.Adapter {
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+    /**
+     *
+     */
+}
