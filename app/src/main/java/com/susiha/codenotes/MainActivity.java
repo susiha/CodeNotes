3fe0b91a986e4a6af0f09314944a23a554dd266e
sunshihai <shihaisun@creditease.cn>
2016-10-21 13:46:47 +0800

    add recycleView

--- app/src/main/java/com/susiha/codenotes/MainActivity.java
+++ app/src/main/java/com/susiha/codenotes/MainActivity.java
@@ -2,12 +2,20 @@ package com.susiha.codenotes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.RecycledViewPool pools;
+    private RecyclerView.Recycler recycle;
+    private RecyclerView.RecyclerListener recyclerListener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView= (RecyclerView) findViewById(R.id.recyclerView);
     }
 }
